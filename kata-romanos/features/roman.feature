731547065ae3404a22176aa3db478c41126cb3d6
Dan Torres <rinox10@gmail.com>
2012-07-29 16:09:48 -0500

    romano 5

--- kata-romanos/features/roman.feature
+++ kata-romanos/features/roman.feature
@@ -14,3 +14,10 @@ Scenario Outline: Convertir <arabigo>
 	| 2		  | II 	    |
 	| 3		  | III     |
 	| 4		  | IV 	    |
+	| 5		  | V 	    |
+	| 6		  | VI 	    |
+	| 7		  | VII	    |
+	| 8		  | VIII    |
+	| 9		  | IX	    |
+	| 10	  | X 	    |
+	
---
+++ kata-romanos/features/step_definitions/roman_steps.rb
@@ -0,0 +1,23 @@
+require 'rspec/expectations'
+
+Given /^un conversor$/ do
+  @conversor = Conversor.new
+end
+
+When /^le paso (\d+)$/ do |numero|
+  @resultado = @conversor.convertir_romano(numero.to_i)
+end
+
+Then /^lo convierte a ([A-Z]*)/ do |valor_romano|
+  @resultado.should == valor_romano
+end
+
+class Conversor
+	def convertir_romano(numero)
+		return "I" if numero == 1
+		return "II" if numero == 2
+		return "III" if numero == 3
+		return "IV" if numero == 4
+		return "V" if numero == 5
+	end
+end
